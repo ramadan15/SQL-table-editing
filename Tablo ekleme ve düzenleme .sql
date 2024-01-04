c1ea145f12a55fa64851fd0d7b9352ebfd7b78b6
ramadan15 <ramazanaltinisik16@gmail.com>
2024-01-04 21:16:56 +0300

    SQL database table editing committed

---
+++ Tablo ekleme ve düzenleme .sql
@@ -0,0 +1,72 @@
+-- 1. Aşama 
+CREATE TABLE employee (
+	id INT,
+	name VARCHAR(50),
+	birthday DATE,
+	email VARCHAR(100))
+-- 2. aşama 
+insert into employee (id, name , email , birthday) values (1, 'Darya', 'dovize0@fda.gov', '9/8/2023');
+insert into employee (id, name , email , birthday) values (2, 'Amabel', 'arochester1@discuz.net', '2/24/2023');
+insert into employee (id, name , email , birthday) values (3, 'Micah', 'menden2@craigslist.org', '12/28/2023');
+insert into employee (id, name , email , birthday) values (4, 'Patrica', 'pneilands3@edublogs.org', '7/20/2023');
+insert into employee (id, name , email , birthday) values (5, 'Morey', 'mhabbergham4@google.com.hk', '5/28/2023');
+insert into employee (id, name , email , birthday) values (6, 'Quinta', 'qberthelet5@prnewswire.com', '7/10/2023');
+insert into employee (id, name , email , birthday) values (7, 'Tris', 'tfilkov6@state.tx.us', '6/13/2023');
+insert into employee (id, name , email , birthday) values (8, 'Lynnett', 'lkillingworth7@usa.gov', '9/29/2023');
+insert into employee (id, name , email , birthday) values (9, 'Cort', 'cgladtbach8@nhs.uk', '10/30/2023');
+insert into employee (id, name , email , birthday) values (10, 'Rockwell', 'rallwright9@shinystat.com', '9/4/2023');
+insert into employee (id, name , email , birthday) values (11, 'Clerc', 'cradsdalea@who.int', '8/31/2023');
+insert into employee (id, name , email , birthday) values (12, 'Dallon', 'dwilflingerb@tiny.cc', '10/15/2023');
+insert into employee (id, name , email , birthday) values (13, 'Arlen', 'agilpinc@psu.edu', '10/2/2023');
+insert into employee (id, name , email , birthday) values (14, 'Rosalinda', 'rskillingtond@omniture.com', '7/1/2023');
+insert into employee (id, name , email , birthday) values (15, 'Falkner', 'favramovitze@ucoz.com', '2/23/2023');
+insert into employee (id, name , email , birthday) values (16, 'Consalve', 'carangyf@sina.com.cn', '12/26/2023');
+insert into employee (id, name , email , birthday) values (17, 'Lucky', 'lsisslandg@utexas.edu', '3/20/2023');
+insert into employee (id, name , email , birthday) values (18, 'Melicent', 'mgreatbankh@theglobeandmail.com', '7/20/2023');
+insert into employee (id, name , email , birthday) values (19, 'Biddy', 'btuffieldi@dyndns.org', '11/16/2023');
+insert into employee (id, name , email , birthday) values (20, 'Candy', 'ckneathj@umich.edu', '2/6/2023');
+insert into employee (id, name , email , birthday) values (21, 'Jammal', 'jmenilovek@slashdot.org', '4/26/2023');
+insert into employee (id, name , email , birthday) values (22, 'Kris', 'kcapelll@cmu.edu', '3/13/2023');
+insert into employee (id, name , email , birthday) values (23, 'Audi', 'areckhousem@hibu.com', '7/20/2023');
+insert into employee (id, name , email , birthday) values (24, 'Hermione', 'hdeenern@foxnews.com', '6/23/2023');
+insert into employee (id, name , email , birthday) values (25, 'Alyosha', 'aofeno@tuttocitta.it', '8/24/2023');
+insert into employee (id, name , email , birthday) values (26, 'Veronike', 'vcouparp@bloglovin.com', '10/14/2023');
+insert into employee (id, name , email , birthday) values (27, 'Mic', 'mbartellq@theglobeandmail.com', '9/6/2023');
+insert into employee (id, name , email , birthday) values (28, 'Homerus', 'herickssenr@devhub.com', '8/13/2023');
+insert into employee (id, name , email , birthday) values (29, 'Arlyn', 'agumerys@linkedin.com', '8/26/2023');
+insert into employee (id, name , email , birthday) values (30, 'Veronique', 'vkinghamt@sfgate.com', '3/27/2023');
+insert into employee (id, name , email , birthday) values (31, 'Nehemiah', 'nokynsillagheu@ebay.com', '9/28/2023');
+insert into employee (id, name , email , birthday) values (32, 'Karee', 'kdotv@spotify.com', '2/4/2023');
+insert into employee (id, name , email , birthday) values (33, 'Pasquale', 'pdukesburyw@blogs.com', '6/25/2023');
+insert into employee (id, name , email , birthday) values (34, 'Jessee', 'jlivockx@yahoo.co.jp', '5/16/2023');
+insert into employee (id, name , email , birthday) values (35, 'Ettie', 'ehawkeridgey@go.com', '7/31/2023');
+insert into employee (id, name , email , birthday) values (36, 'Beilul', 'bcraggsz@booking.com', '10/17/2023');
+insert into employee (id, name , email , birthday) values (37, 'Leandra', 'lpybworth10@usa.gov', '2/18/2023');
+insert into employee (id, name , email , birthday) values (38, 'Walden', 'wtatnell11@cyberchimps.com', '3/10/2023');
+insert into employee (id, name , email , birthday) values (39, 'Atlanta', 'amaccaughey12@yellowbook.com', '10/4/2023');
+insert into employee (id, name , email , birthday) values (40, 'Franklin', 'ffooter13@1und1.de', '1/12/2023');
+insert into employee (id, name , email , birthday) values (41, 'Joy', 'jbuckleigh14@senate.gov', '10/7/2023');
+insert into employee (id, name , email , birthday) values (42, 'Shalne', 'sbrumble15@4shared.com', '5/7/2023');
+insert into employee (id, name , email , birthday) values (43, 'Linet', 'llaxston16@google.es', '9/28/2023');
+insert into employee (id, name , email , birthday) values (44, 'Camile', 'cmcgaugie17@wikispaces.com', '11/2/2023');
+insert into employee (id, name , email , birthday) values (45, 'Carey', 'chaseley18@tiny.cc', '12/19/2023');
+insert into employee (id, name , email , birthday) values (46, 'Babara', 'bcarreck19@weibo.com', '2/2/2023');
+insert into employee (id, name , email , birthday) values (47, 'Robby', 'rdouble1a@cpanel.net', '4/7/2023');
+insert into employee (id, name , email , birthday) values (48, 'Corinna', 'ctraviss1b@networksolutions.com', '5/15/2023');
+insert into employee (id, name , email , birthday) values (49, 'Gabriell', 'gbarby1c@blogspot.com', '11/28/2023');
+insert into employee (id, name , email , birthday) values (50, 'Brigitta', 'bgrishin1d@prweb.com', '7/16/2023');
+
+-- 3. Aşama
+update employee 
+set name= 'Ramazan'
+where id= 2
+
+-- 4. aşama
+Update employee 
+set name = 'Rammy'
+where birthday= '2023-09-08'
+
+-- 5. Aşama
+Delete from employee 
+where id= 5 
+
